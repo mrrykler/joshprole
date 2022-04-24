90604a836bb65e52860e86c061735a590c7b14b4
unknown <joshblasy@gmail.com>
2022-04-23 20:53:22 -0500

    modified:   joshprole/urls.py

--- joshprole/urls.py
+++ joshprole/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import welcome
+from .views import index
 
 urlpatterns = [
-    path('', welcome)
+    path('', index)
 ]
